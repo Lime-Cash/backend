f0d2b6a917318b3c3e92a22266d09d48c97d57d9
martinbarreiroo <mbarreiro@mail.austral.edu.ar>
2025-04-30 23:00:12 -0300

    added it git hook pre-commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # RESTful resource routes
-  resources :transfers, only: [:create, :index]
-  resources :transactions, only: [:create, :index]
+  resources :transfers, only: [ :create, :index ]
+  resources :transactions, only: [ :create, :index ]
   resources :accounts
-  resources :users, only: [:create, :show]
+  resources :users, only: [ :create, :show ]
 
   # Health check route
   get "up" => "rails/health#show", as: :rails_health_check
