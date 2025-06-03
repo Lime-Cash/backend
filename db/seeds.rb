6eb67612a416d28a48d9bd157e8aad10a292af50
ManuLosta <mlostalom@gmail.com>
2025-06-03 17:37:15 -0300

    feat: docker compose runs db and server

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-john_user = User.create!(name: "John Doe", email: "johndoe@mail.com", password: "password")
-mary_user = User.create!(name: "Mary Doe", email: "marydoe@mail.com", password: "password")
+if Rails.env.development?
+  john_user = User.create!(name: "John Doe", email: "johndoe@mail.com", password: "password")
+  mary_user = User.create!(name: "Mary Doe", email: "marydoe@mail.com", password: "password")
 
-mary_account = mary_user.create_account(balance: 100)
-john_account = john_user.create_account(balance: 100)
+  mary_account = mary_user.create_account(balance: 100)
+  john_account = john_user.create_account(balance: 100)
 
-Transfer.create!(from_account: john_account, to_account: mary_account, amount: 10)
-Transfer.create!(from_account: mary_account, to_account: john_account, amount: 10)
+  Transfer.create!(from_account: john_account, to_account: mary_account, amount: 10)
+  Transfer.create!(from_account: mary_account, to_account: john_account, amount: 10)
+
+  puts "Seeded development data 🎉"
+else
+  puts "Skipping seeds — not in development environment."
+end
